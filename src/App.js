c14e5ee0958ebc4ff9febde0ff355c11281f9707
jwy5140 <jwy5140@gmail.com>
2019-08-27 04:36:12 -0400

    Login form/front page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
+import styled from 'styled-components'
 
 import Routes from './routes'
 
 function App() {
   return (
-    <div className="App">
+    <Wrapper>
       <Routes />
-      <style jsx>{`
-
-      `}
-      </style>
-    </div>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100vw;
+`
--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,20 +1,130 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleLogin = () => {
+        return null
+    }
+
     return (
-        <div className='login'>
-            <img className='front-page-splash' src='./images/belart84-unsplash.jpg' alt='belart84-unsplash-fruit' />
-            <div className='login-block'>
+        <FrontPage>
+            <Splash>
+                <Image src='./images/belart84-unsplash.jpg' alt='belart84-unsplash-fruit' />
+            </Splash>
+            <LoginBlock>
                 <h1>Grocery Basket</h1>
-            </div>
-            <style jsx>{`
-                img.front-page-splash {
-                    margin: 0 50vw 0 10vw;
-                    max-height: 100vh;
-                }
-            `}
-            </style>
-        </div>)
+                <input type='text' value={username} onChange={e=>setUsername(e.target.value)} placeholder='Username' />
+                <input type='password' value={password} onChange={e=>setPassword(e.target.value)} placeholder='Password' />
+                <button onClick={handleLogin}>LOGIN</button>
+                <div>Forgot your username or password? <Link>Click here.</Link></div>
+                <div>Need an account? <Link>Sign Up</Link></div> 
+                <div className='login-footer'>
+                    <Link>About Us</Link>
+                    <Link>Contact Us</Link>
+                    <Link>Github</Link>
+                </div>
+            </LoginBlock>
+        </FrontPage>)
 }
 
 export default Login
+
+
+//Styles
+
+const FrontPage = styled.div`
+    margin: 0 auto 0 auto;
+    display: flex;
+    flex-direction: row;
+`
+
+const Splash = styled.div`
+    display: inline-block;
+`
+
+const Image = styled.img`
+    margin-left: 15vw;
+    max-height: 100vh;
+`
+
+const LoginBlock = styled.div`
+    background-color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: auto auto auto 10vw;
+    height: 50vh;
+    border: 1px solid black;
+
+    a {
+        color: #63C7EC;
+
+        &:visited {
+            color: #63C7EC;
+        }
+    }
+
+    h1 {
+        font-family: 'Fredericka the Great', cursive;
+        padding: 0 5vw 2.5vh 5vw;
+    }
+
+    input {
+        margin: 1vh 0 1vh 0;
+        padding-left: 5px;  
+        width: 50%;
+        height: 5%;
+        border: 1px solid black;
+        border-radius: 5px;
+        background-color: whitesmoke;
+
+        &:focus {
+            outline: none;
+        }
+    }
+
+    button {
+        margin: 2vh 0 1vh 0;
+        padding-left: 5px;  
+        width: 50%;
+        height: 5%;
+        border: none;
+        border-radius: 5px;
+        font-weight: bold;
+        color: white;
+        background-color: #63C7EC;
+
+        &:hover {
+            background-color: #5AB0D0;
+        }
+    }
+
+    div {
+        margin-top: 2vh;
+        font-size: .75em;
+        a {
+            text-decoration: none;
+            font-weight: bold;
+        }
+    }
+
+    .login-footer {
+        margin-top: auto;
+        margin-bottom: 1vh;
+
+        a {
+            border-right: 1px solid black;
+            padding-right: .5vw;
+            margin-right: .5vw;
+        }
+        
+        a:last-child {
+            border-right: none;
+        }
+    }
+`
